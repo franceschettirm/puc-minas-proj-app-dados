4cfdffdd4ee71ff262fa488559222c556698da47
franceschettirm <franceschettirm@gmail.com>
2023-06-05 22:24:28 -0300

    incluindo arquivo com definições dos caminhos

---
+++ app/definitions.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+DATASET_FOLDER_PATH = os.path.join(ROOT_PATH, "files")
+CONFIG_FILE_PATH = os.path.join(ROOT_PATH, "config.yaml")
